9bce4c038e097b78b04912c6a41271125ccb1bbf
Rodney <42125381+rodneymthunzi@users.noreply.github.com>
2026-05-31 13:34:35 +0200

    Add files via upload

---
+++ database/001_create_schemas.sql
@@ -0,0 +1,31 @@
+/*
+Goal:    Creating schema objects to serve various functions.
+
+		 rpt: Reporting
+		 sync: Data sync
+		 loadData: Loading data
+		 dim: Dimensions
+		 fact: Fact tables
+
+Author:  Rodney Zhou
+
+Project: MiniPOS
+*/
+
+USE MiniPOS
+GO
+
+CREATE SCHEMA rpt 
+GO
+
+CREATE SCHEMA sync 
+GO
+
+CREATE SCHEMA loadData 
+GO
+
+CREATE SCHEMA dim
+GO
+
+CREATE SCHEMA fact
+GO
