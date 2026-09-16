cb2f3bd854e5f1e02eb40b6a1bd91d64e722beba
Rodney <42125381+rodneymthunzi@users.noreply.github.com>
2026-09-16 05:08:49 +0200

    Update pilot_covers.sql

--- reports/pilot_covers.sql
+++ reports/pilot_covers.sql
@@ -4,10 +4,14 @@ DECLARE @fromDate date = '2026-07-05'
 DECLARE @toDate date = '2026-07-05'
 SELECT  CAST(INVDATE as date) BusinessDate
 		,@unit Unit
-		,CASE WHEN @unit like '%Golden Valley%' THEN 'GV - ' WHEN @unit like '%flamingo%' THEN 'FM - ' WHEN @unit like '%Windmill%' THEN 'WM - ' END +RevCentre Outlet
+		,CASE 
+			WHEN @unit like '%Golden Valley%' THEN 'GV - ' 
+			WHEN @unit like '%flamingo%' THEN 'FM - ' 
+			WHEN @unit like '%Windmill%' THEN 'WM - ' 
+		END +RevCentre Outlet
 		,SUM(sm.CUST) NumberOfGuest
 		,'3' Source
 FROM dbo.SALESDETAIL sd
 JOIN dbo.SALESMASTER sm ON sd.INVDATE=sm.IDATE AND sd.OUTM=sm.OUTM
-WHERE sd.INVDATE BETWEEN @fromDate AND @toDate
+WHERE sd.INVDATE BETWEEN @fromDate AND @toDate AND sd.DTAB <> 'MODIFY'
 GROUP BY INVDATE, RevCentre
