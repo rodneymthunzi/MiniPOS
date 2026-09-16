f37711863709bc01231794db92368a22693a9f18
Rodney <42125381+rodneymthunzi@users.noreply.github.com>
2026-09-16 05:03:29 +0200

    Update pilot_tenders.sql

--- reports/pilot_tenders.sql
+++ reports/pilot_tenders.sql
@@ -6,7 +6,9 @@ DECLARE @toDate DATE = '2026-07-05'
 SELECT 
 	CAST(TranDate As Date) BusinessDate
 	,@unit Unit
-	,CASE WHEN @unit like '%Golden Valley%' THEN 'GV - ' WHEN @unit like '%flamingo%' THEN 'FM - ' WHEN @unit like '%Windmill%' THEN 'WM - ' END +RevCentre Outlet
+	,CASE 
+		WHEN @unit like '%Golden Valley%' THEN 'GV - ' WHEN @unit like '%flamingo%' THEN 'FM - ' 
+		WHEN @unit like '%Windmill%' THEN 'WM - ' END + RevCentre Outlet
 	,vp.CasName Waitron
 	,vp.Amount
 	,vp.Info TenderMediaType
